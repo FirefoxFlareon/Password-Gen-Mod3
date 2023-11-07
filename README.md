15f1a12d20d0a37c29a3a67e811cd788defa83ec
Firefox <tshankgg@gmail.com>
2023-11-07 15:09:02 -0600

    Final push, assignment completed

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Challenge-3
-Password Generator
+# Module 3 Challenge
+
+## Description
+This password generator allows users to create a new randomly generated password. 
+Users will be promted for a desired length as well as which character types they would like included.
+Once all prompts have been answered the randomly generated password will be displayed.
+
+## Screenshot
+![Screenshot](/assets/Screenshot%202023-11-07%20150500.png)
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var special = "!#$%&'()*+-./:<=>?@[]^,_`{|}";
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -11,12 +14,52 @@ function writePassword() {
 }
 
 function generatePassword() {
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-';
-    var password = "";
-    
+  var passwordLength = prompt("Enter desired length of password (between 8 and 128 characters)");
 
+  if (
+    passwordLength === null ||
+    isNaN(passwordLength) ||
+    passwordLength < 8 ||
+    passwordLength > 128
+  ) {
+    alert("Please enter a valid length between 8 and 128 characters.");
+    return "";
+  }
+
+  var password = "";
+  var charPool = "";
+
+  var includeLowerCase = confirm("Include lowercase characters?");
+  var includeUpperCase = confirm("Include uppercase characters?");
+  var includeNumbers = confirm("Include numbers?");
+  var includeSpecial = confirm("Include special characters?");
+
+  if (includeLowerCase) {
+    charPool += lowerCase;
+  }
+  if (includeUpperCase) {
+    charPool += upperCase;
+  }
+  if (includeNumbers) {
+    charPool += numbers;
+  }
+  if (includeSpecial) {
+    charPool += special;
+  }
+
+
+  if (charPool === "") {
+    alert("Please select at least one character type for your password.");
+    return "";
+  }
+
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomBank = Math.floor(Math.random() * charPool.length);
+    password += charPool.charAt(randomBank);
+  }
+
+  return password;
 }
 
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
